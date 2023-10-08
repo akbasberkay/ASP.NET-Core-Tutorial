205cbc67a1c2c78b4211ce0fed337c568292b3c2
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-10-08 23:08:59 +0300

    new commit
    new commit

---
+++ E-Commerce Web App/E-Commerce Web App/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_Web_App.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int ShowOrder { get; set; }
+    }
+}
